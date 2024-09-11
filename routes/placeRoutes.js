d87c4757cbe1748155ab19fb48e35604b07b6ebc
SantiagoMadrid <santiagomadrid2020@gmal.com>
2024-09-11 15:14:38 -0500

    logica imagenes del home

--- routes/placeRoutes.js
+++ routes/placeRoutes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { searchIA, infoDestino, detallesLugar, detallesCard } from '../controllers/aiController.js';
+import { searchIA, infoDestino, detallesLugar, detallesCard, lugaresPorIds } from '../controllers/aiController.js';
 import { register, login } from '../controllers/loginController.js';
 import hashMiddleware from '../middleware/hashMiddleware.js'
 
@@ -9,6 +9,7 @@ router.post('/busquedaIA', searchIA);
 router.post('/infoDestino', infoDestino);
 router.post('/detallesLugar', detallesLugar);
 router.post('/detallesCard', detallesCard);
+router.post('/lugaresPorIds', lugaresPorIds);
 router.post('/registro', hashMiddleware, register);
 router.post('/login', login);
 
