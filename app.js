1749f58aad98d4d78d83e77a06efd5becaca21c3
= <josefergo89@gmail.com>
2024-09-29 18:34:13 -0500

    corsOrigin

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const corsOptions = {
 }
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://proyectofinalsena.onrender.com');  // Origen permitido
+    res.header('Access-Control-Allow-Origin', ['https://proyectofinalsena.onrender.com', 'http://localhost:8080']);  // Origen permitido
     res.header('Access-Control-Allow-Credentials', 'true');  // Permitir credenciales
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');  // Métodos permitidos
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');  // Cabeceras permitidas
